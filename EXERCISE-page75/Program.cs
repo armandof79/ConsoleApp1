8b0ff1e52b09ba7b0632ebca5cc928e6b54d44c8
armandof79 <a.ferreiras@gmail.com>
2019-12-06 15:38:07 -0500

    New assigment

---
+++ EXERCISE-page75/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCISE_page75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? type true or false");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool qualify = age >= 15 && dui == false && tickets <= 3;
+            Console.WriteLine("Do you qualify?: " + qualify);
+            Console.ReadLine();
+        }
+    }
+}
