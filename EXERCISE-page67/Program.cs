46a17dabd5af8400d98f63e051962904bf92df20
armandof79 <a.ferreiras@gmail.com>
2019-12-06 13:59:09 -0500

    Update

---
+++ EXERCISE-page67/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace EXERCISE_page67
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Enter hourly rate");
+            decimal a = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter hours worked per week");
+            decimal b = Convert.ToDecimal(Console.ReadLine());
+            decimal person1 = a * b;
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Enter hourly rate");
+            decimal c = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter hours worked per week");
+            decimal d = Convert.ToDecimal(Console.ReadLine());
+            decimal person2 = c * d;
+            bool compareSalary = person1 > person2;
+            Console.WriteLine("Does Person #1 make more money than Person #2? " + compareSalary);
+            Console.Read();
+
+        }
+    }
+}
